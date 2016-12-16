313720c720f7e8d1b0e6e1a14cc46e46bcd4f597
HEBATALLA TAREK <heba.tarek@rocketmail.com>
2016-12-16 16:47:38 +0200

    Update newtonRaphSolver.m

--- newtonRaphSolver.m
+++ newtonRaphSolver.m
@@ -2,34 +2,85 @@ classdef newtonRaphSolver < solver
     
     %Use this as a template for later class implementations
     properties (Constant)
-        reqLabels = [{'Xu'},{'Xl'}];
+        reqLabels = [{'Xi'}];
         dataLabels = [{'Xi'},{'Xi+1'}];
     end
     
-    properties
-        equation
-        r1
-        r2
+    properties(SetAccess = protected)
+        eqn
+        x1=obj.currentValue % represent xip1
+        y1=obj.eqn(x1)
+        x2=obj.lastValue% represent xi
+        %ep = 1e-9;
+        fdx1= (obj.eqn(x1+ep)-obj.eqn(x1-ep))/(2*ep)% derivative of X1
+        y2=x1-(y1/fdx1)
+        tangent=y1+(x-x1)*fdx1
+        
     end
     
     methods (Access = protected)
         function step(obj)
             % Do one iteration
-            % Update obj.currentValue, obj.lastValue, obj.stateData            
+            
+            xi = obj.lastValue;
+            fxi = obj.eqn(xi);
+            ep = 1e-9;
+            fdxi= (obj.eqn(xi+ep)-obj.eqn(xi-ep))/(2*ep);
+            xip1 = xi - (fxi) / (fdxi);
+            fxip1 = obj.eqn(xip1);
+            %% Update state% Update obj.currentValue, obj.lastValue, obj.stateData  
+            obj.currentValue = xip1;
+            
+            obj.stateData = [ xi xip1  fxi fdxi obj.getAppError()];            
+            %% Set plotting range
+            
+                      
         end
     end
     
     methods
-        function obj = newtonRaphSolver(equation, req)
+        function obj = newtonRaphSolver(eqn, req)
             % req contain requirements
             obj.totalTime = 0;
-            obj.equation = equation;
-            obj.r1 = req(1);
-            obj.r2 = req(2);
+            obj.eqn = eqn;
+            obj.lastValue = req(1); % set xi 
+            
         end
+          
+        
         function plotState(obj)
             % For demonstration only
-            ezplot(obj.equation);
+            range = abs(obj.x1-obj.x2);
+            y = zeros(1,length(range));
+            for i = 1:length(y)
+                y(i) = obj.eqn(range(i));
+                
+                
+            end
+            
+            
+            title('Newton''s Method Solution: ');
+            xlabel('Iteration','FontSize',16)
+ylabel('$x$','FontSize',16,'Interpreter','latex')
+            plot(range, y, 'b', [obj.X2 obj.X1], [obj.eqn(obj.X2) obj.eqn(obj.X1)], 'r'...
+                , obj.currentValue, obj.eqn(obj.currentValue), 'g-o');
+            
+            
+                
+            
+            hold on;
+            pad=0.1*range;
+            if(obj.x1>obj.x2)
+            
+            ezplot(obj.tangent, [obj.x2-pad,obj.x1+pad])
+            end
+            if(obj.x1<obj.x2)
+            
+            ezplot(obj.tangent, [obj.x1-pad,obj.x2+pad])
+            end
+
+ hold off;
+            
         end
     end
     
