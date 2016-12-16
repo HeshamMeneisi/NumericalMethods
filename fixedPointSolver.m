66d3cb6ce8b9481f0a255f50757afd1b3f62e2c4
Nader Alsharkawy <naderalsharkawy@gmail.com>
2016-12-16 16:40:10 +0200

    Implement Fixed Point technique

--- fixedPointSolver.m
+++ fixedPointSolver.m
@@ -2,20 +2,21 @@ classdef fixedPointSolver < solver
     
     %Use this as a template for later class implementations
     properties (Constant)
-        reqLabels = [{'Xu'},{'Xl'}];
-        dataLabels = [{'Xi'},{'Xi+1'}];
+        reqLabels = {'X0'};
+        dataLabels = [{'Xi+1'},{'Xi'},{'ep'}];
     end
     
     properties
-        equation
-        r1
-        r2
+        f
     end
     
     methods (Access = protected)
         function step(obj)
             % Do one iteration
+            obj.lastValue = obj.currentValue;
+            obj.currentValue = obj.f(obj.currentValue); 
             % Update obj.currentValue, obj.lastValue, obj.stateData            
+            obj.stateData=[obj.currentValue obj.lastValue obj.getAppError()];            
         end
     end
     
@@ -23,13 +24,18 @@ classdef fixedPointSolver < solver
         function obj = fixedPointSolver(equation, req)
             % req contain requirements
             obj.totalTime = 0;
-            obj.equation = equation;
-            obj.r1 = req(1);
-            obj.r2 = req(2);
+            obj.f = equation;
+            obj.currentValue = req(1);
         end
         function plotState(obj)
-            % For demonstration only
-            ezplot(obj.equation);
+            fplot(obj.f);
+            hold on;
+            range = obj.currentValue:0.05*abs(obj.currentValue-obj.lastValue):obj.lastValue;
+            y = zeros(1,length(range));
+            for i = 1:length(y)
+                y(i) = obj.f(range(i));
+            end
+            plot(range, y, 'b', obj.currentValue,obj.f(obj.currentValue), 'r-x');
         end
     end
     
